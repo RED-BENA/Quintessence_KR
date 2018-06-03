643dd78fe1bf2ec98039cb012f16e66996ce5f39
RED-BENA <dlekrud0503@naver.com>
2018-06-03 17:54:48 +0900

    #1 Add Slide-box and Javascript about sliding

--- js/script.js
+++ js/script.js
@@ -22,4 +22,25 @@ $(document).ready(function() {
   }, function() {
     $('.js-category').removeClass('open');
   });
+
+  $(".js-banner__slide-box__indicator li").click(function() {
+    let index = $(this).index();
+
+    $(".js-banner__slide-box__indicator li").removeClass('active');
+    $(this).addClass('active');
+
+    switch(index) {
+      case 0:
+        $('.js-banner__slide-box__list').css('margin-left', '0px');
+        break;
+      case 1:
+        $('.js-banner__slide-box__list').css('margin-left', '-740px');
+        break;
+      case 2:
+        $('.js-banner__slide-box__list').css('margin-left', '-1480px');
+        break;
+      case 3:
+        $('.js-banner__slide-box__list').css('margin-left', '-2220px');
+    }
+  });
 });
