549a8a22e5cfedf6f3678f5c211d9936c865cc0c
RED-BENA <dlekrud0503@naver.com>
2018-06-03 20:35:07 +0900

    #3 Add new book slide-box and Modify banner automatically

--- js/script.js
+++ js/script.js
@@ -43,4 +43,38 @@ $(document).ready(function() {
         $('.js-banner__slide-box__list').css('margin-left', '-2220px');
     }
   });
+
+  setInterval(function() {
+
+    // active 상태의 slide-box item의 다음 인덱스
+    let next_index = $(".js-banner__slide-box__indicator li.active").index()+1;
+
+    switch(next_index) {
+      case 1:
+        $('.js-banner__slide-box__list').css('margin-left', '-740px');
+        break;
+      case 2:
+        $('.js-banner__slide-box__list').css('margin-left', '-1480px');
+        break;
+      case 3:
+        $('.js-banner__slide-box__list').css('margin-left', '-2220px');
+        break;
+      case 4:
+        $('.js-banner__slide-box__list').css('margin-left', '0px');
+        next_index = 0;
+    }
+
+    $(".js-banner__slide-box__indicator li").removeClass('active');
+    $(".js-banner__slide-box__indicator li:nth-child("+(next_index+1)+")").addClass('active');
+  }, 3000);
+
+  // new book slide-box prev/next event
+
+  $('.js-new-book__slide-box__prev').click(function() {
+
+  });
+
+  $('.js-new-book__slide-box__next').click(function() {
+    
+  });
 });
