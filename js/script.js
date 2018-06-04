72e6324b8a3f82f153e64ff091ee781a603db182
RED-BENA <dlekrud0503@naver.com>
2018-06-04 23:42:08 +0900

    #3 Add prev/next button event

--- js/script.js
+++ js/script.js
@@ -23,6 +23,7 @@ $(document).ready(function() {
     $('.js-category').removeClass('open');
   });
 
+  // indicator를 누르면 해당 이미지로 이동하는 event
   $(".js-banner__slide-box__indicator li").click(function() {
     let index = $(this).index();
 
@@ -44,9 +45,11 @@ $(document).ready(function() {
     }
   });
 
+  // banner automatically (자동화)
   setInterval(function() {
 
     // active 상태의 slide-box item의 다음 인덱스
+    // (index는 0부터, nth-child는 1부터 계산하기 때문에 index에 먼저 1을 더해준다.)
     let next_index = $(".js-banner__slide-box__indicator li.active").index()+1;
 
     switch(next_index) {
@@ -68,13 +71,46 @@ $(document).ready(function() {
     $(".js-banner__slide-box__indicator li:nth-child("+(next_index+1)+")").addClass('active');
   }, 3000);
 
-  // new book slide-box prev/next event
-
+  // new book slide-box의 prev 버튼을 클릭했을 때
   $('.js-new-book__slide-box__prev').click(function() {
+
+    // new book slide-box에 보여지는 도서 중에서 첫 번째 도서의 index
+    // (index는 0부터, nth-child는 1부터 계산하기 때문에 index에 먼저 1을 더해준다.)
+    let first_index = $('.js-new-book__slide-box__list li.first').index()+1;
+
+    // first_index가 전체 도서 중 첫 번째 도서의 index일 경우
+    if (first_index == 1) {
+      first_index = 7; // 두 번째 보여지는 도서(7 ~ 12)의 첫 번째 인덱스(7)로 세팅
+    }
+
+    // 전체 도서의 first 클래스 remove
+    // 현재 first_index의 이전 도서(first_index - 1)에 first 클래스 add
+    $('.js-new-book__slide-box__list li').removeClass('first');
+    $('.js-new-book__slide-box__list li:nth-child('+(first_index - 1)+')').addClass('first');
+
+    // 현재 first_index에서 왼쪽으로 한 칸(first_index - 1) 이동
+    $('.js-new-book__slide-box__list').css('margin-left', ((first_index - 1) * -156)+'px');
 
   });
 
+  // new book slide-box의 next 버튼을 클릭했을 때
   $('.js-new-book__slide-box__next').click(function() {
-    
+
+    // new book slide-box에 보여지는 도서 중에서 첫 번째 도서의 index
+    // (index는 0부터, nth-child는 1부터 계산하기 때문에 index에 먼저 1을 더해준다.)
+    let first_index = $('.js-new-book__slide-box__list li.first').index()+1;
+
+    // first_index가 전체 도서 중 마지막 도서의 index일 경우
+    if (first_index == 12) {
+      first_index = 1; // 첫 번째 보여지는 도서(1 ~ 6)의 첫 번째 인덱스(1)로 세팅
+    }
+
+    // 전체 도서의 first 클래스 remove
+    // 현재 first_index의 다음 도서(first_index + 1)에 first 클래스 add
+    $('.js-new-book__slide-box__list li').removeClass('first');
+    $('.js-new-book__slide-box__list li:nth-child('+(first_index + 1)+')').addClass('first');
+
+    // 현재 first_index에서 오른쪽으로 한 칸(first_index + 1) 이동
+    $('.js-new-book__slide-box__list').css('margin-left', ((first_index + 1) * -156)+'px');
   });
 });
