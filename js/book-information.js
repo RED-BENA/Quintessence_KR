020fa805061acd39735ac90794d52491fd0d62b0
RED-BENA <dlekrud0503@naver.com>
2018-07-16 11:48:43 +0900

    #14 Apply CSS 배송/반품/교환/AS (detail4)

--- js/book-information.js
+++ js/book-information.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
 
-  // 이용후기 입력하기 버튼 click 이벤트
+  // 이용후기/상품문의 입력하기 버튼 click 이벤트
 
-  $('.js-book-information__detail2__add-review').click(function() {
-    $('.js-book-information__detail2__review-container').toggle();
+  $('.js-book-information__detail__add-review').click(function() {
+    $('.js-book-information__detail__review-container').toggle();
+  });
+
+  $('.js-book-information__detail__add-qna').click(function() {
+    $('.js-book-information__detail__qna-container').toggle();
   })
 
   // 내용보기 버튼 click 이벤트
