20d6dfabdc618f854339dde036ca5210f93fe13f
RED-BENA <dlekrud0503@naver.com>
2018-11-05 15:48:52 +0900

    #16 Finish renewing classname of book-information

--- js/book-information.js
+++ js/book-information.js
@@ -2,15 +2,15 @@ $(document).ready(function() {
 
   // 이용후기/상품문의 입력하기 버튼 click 이벤트
 
-  $('.js-book-information-detail__add-review').click(function() {
-    $('.js-book-information-detail__review').toggle();
+  $('.js-book-information-detail2__add-review').click(function() {
+    $('.js-book-information-detail2__review').toggle();
   });
 
-  $('.js-book-information-detail__add-qna').click(function() {
-    $('.js-book-information-detail__qna').toggle();
+  $('.js-book-information-detail3__add-qna').click(function() {
+    $('.js-book-information-detail3__qna').toggle();
   })
 
-  // 내용보기 버튼 click 이벤트
+  // 전체내용 버튼 click 이벤트
 
   $('.js-review-list__open').click(function() {
     var index = $(this).parent().parent().parent().index(); // .js-book-information__detail__review li의 인덱스 번호
@@ -21,6 +21,6 @@ $(document).ready(function() {
   $('.js-qna-list__open').click(function() {
     var index = $(this).parent().parent().parent().index(); // .js-book-information__detail__qna li의 인덱스 번호
     $('.js-qna-list__item:nth-child('+(index+1)+') .js-qna-list__summary').toggleClass('open');
-    $('.js-qna-list__item:nth-child('+(index+1)+') .js-qna-list__detail-qna').toggle();
+    $('.js-qna-list__item:nth-child('+(index+1)+') .js-qna-list__whole-qna').toggle();
   });
 });
